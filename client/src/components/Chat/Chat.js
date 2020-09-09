89ae3c1fd48c563610703843ef442d5f3a061570
osama865 <60786545+osama865@users.noreply.github.com>
2020-09-09 07:06:32 -0700

    updating endpoint
    updating endpoint to the deployed back-end  in heroku

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -17,7 +17,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState([]);
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = "localhost:5000";
+  const ENDPOINT = "https://chat-o.herokuapp.com/";
   useEffect(() => {
     const { Name, Room } = queryString.parse(location.search);
 
